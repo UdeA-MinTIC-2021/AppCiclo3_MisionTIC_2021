bf7fffda89ea43ac02312dfc0bffc2bb359363b8
Andrés Suárez <carlosandressuarez16@gmail.com>
2021-12-19 21:02:05 -0500

    coreccion iniciar sesion

--- app_ventas/src/Pages/interface1/login.jsx
+++ app_ventas/src/Pages/interface1/login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
             <span id='Salessoft'><b>SalesSoft</b></span>
             </div>
             <div className="textoIS">
-              <span id="IS"><b>Iniciar sesión en tu cuenta</b></span>
+              <span id="IS"><b>Iniciar sesión</b></span>
             </div>
 
             <form action="" className="formulario">
